f07e9fdfd135306b9d8a71744a944d97b3840ecb
Michaela Schormann <u19014938@tuks.co.za>
2022-06-01 11:51:56 +0200

    Upgraded design

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const SERVER_URL = 'https://eulfffffwfjhh-server.herokuapp.com/'
+// const SERVER_URL = 'https://eulfffffwfjhh-server.herokuapp.com/'
+const SERVER_URL = 'https://cd00-196-36-112-50.eu.ngrok.io/'
 // const SERVER_URL = 'http://localhost:3000/'
 
 const makeScoreCard = (place, title, score) => {
-    return $("<div>", {'class': 'card mb-2'}).append(
-        $("<div>", {'class': 'card-body py-1'}).append([
+    return $("<div>", {'class': 'card mb-2 shadow border-primary'}).append(
+        $("<div>", {'class': 'card-body py-1 '}).append([
                 $("<h3>", {'class': 'd-inline me-4'}).text(`#${place}`),
                 $("<h3>", {'class': 'd-inline'}).text(title),
                 $("<h3>", {'class': 'd-inline float-end'}).text(`${score} Points`)
@@ -13,7 +14,7 @@ const makeScoreCard = (place, title, score) => {
 
 const makeTeamCard = (teamName) => {
     return $("<div>", {'class': 'col-4'}).append(
-        $("<div>", {'class': 'card mb-3 me-1'}).append(
+        $("<div>", {'class': 'card mb-3 me-1 shadow border-primary'}).append(
             $("<div>", {'class': 'card-body py-1'}).append(
                 $("<h3>", {'class': 'text-center'}).text(teamName)
             )
@@ -22,9 +23,14 @@ const makeTeamCard = (teamName) => {
 }
 
 const scoreboard = (game) => {
+
     $("body").append(
         $("<div>", {'class': 'row'}).append(
-            $("<div>", {'class': 'list-group col-5', 'id': 'scoreboard'})
+            $("<div>", {'class': 'list-group col-12 mb-3', 'id': 'scoreboard-heading'}).append(
+                $("<h1>", {'class': 'text-center'}).text("Scoreboard")
+            )
+        ).append(
+            $("<div>", {'class': 'list-group col-6', 'id': 'scoreboard'})
         )
     )
 
@@ -43,7 +49,7 @@ const scoreboard = (game) => {
             .catch(err => {
                 console.log(`Server Error: ${err}`)
                 $("#scoreboard").empty().append(
-                    $("<div>", {'class': 'card mb-2'}).append(
+                    $("<div>", {'class': 'card mb-2 shadow'}).append(
                         $("<div>", {'class': 'card-body py-1'}).append(
                             $("<h3>", {'class': 'text-center'}).text("No scores yet")
                         )
@@ -56,7 +62,11 @@ const scoreboard = (game) => {
 
 const teams = () => {
     $("body").empty().append(
+        $("<div>", {'class': 'list-group col-12 mb-3', 'id': 'teams-heading'}).append(
+            $("<h1>", {'class': 'text-center'}).text("Team Names")
+        ).append(
         $("<div>", {'class': 'row', 'id': 'teams'})
+        )
     )
     const poll = () => {
         fetch(`${SERVER_URL}team_names`)
@@ -73,8 +83,8 @@ const teams = () => {
             .catch(err => {
                 console.log(`Server Error: ${err}`)
                 $("#teams").empty().append(
-                    $("<div>", {'class': 'card mb-2'}).append(
-                        $("<div>", {'class': 'card-body py-1'}).append(
+                    $("<div>", {'class': 'card mb-2 shadow'}).append(
+                        $("<div>", {'class': 'card-body py-1 '}).append(
                             $("<h3>", {'class': 'text-center'}).text("No teams yet")
                         )
                     )
@@ -84,8 +94,24 @@ const teams = () => {
     setInterval(poll, 3000)
 }
 
-const winners = () => {
+const leaderboard = () => {
+    $("body").empty().append(
+        $("<div>", {'class': 'list-group col-12 mb-3', 'id': 'leaderboard-heading'}).append(
+            $("<h1>", {'class': 'text-center'}).text("♚ LEADERBOARD ♚")
+        ).append(
+        $("<div>", {'class': 'row', 'id': 'leaderboard'})
+        )
+    )
+}
 
+const winners = () => {
+    $("body").empty().append(
+        $("<div>", {'class': 'list-group col-12 mb-3 text-warning', 'id': 'winners-heading'}).append(
+            $("<h1>", {'class': 'text-center'}).text("★ WINNERS ★")
+        ).append(
+        $("<div>", {'class': 'row', 'id': 'winners'})
+        )
+    )
 }
 
 const main = () => {   
@@ -116,10 +142,12 @@ const main = () => {
     }
     else if (page == "winners") {
         winners()
+    }else if(page == "leaderboard"){
+        leaderboard()
     }
     else {
         $("body").empty().append(
-            $("<h4>").text("This page does not exists, enter either 'game', 'teams' or 'winners'")
+            $("<h4>").text("This page does not exists, enter either 'game', 'teams', 'winners' or 'leaderboard'")
         )
     }
 }
